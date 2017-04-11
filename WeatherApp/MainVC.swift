466e7605cf870744cd6021171f120581ffa8951c
sherminb <Sherea@apples-MacBook-Pro.local>
2017-04-11 15:59:03 -0600

    Installed almofire and started working on the ui

--- WeatherApp/MainVC.swift
+++ WeatherApp/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  WeatherApp
 //
 //  Created by Kuala on 2017-04-11.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
