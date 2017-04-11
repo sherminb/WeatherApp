b2efd7c02604d62d95efd3183cfba908c00c237e
sherminb <Sherea@apples-MacBook-Pro.local>
2017-04-11 16:32:53 -0600

    Getting ready to read data from weather api

--- WeatherApp/Constants.swift
+++ WeatherApp/Constants.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
-let BASE_URL="https"
-let APPLICATION_ID=""
+let BASE_URL = "http://samples.openweathermap.org/data/2.5/weather?lat="
+let API_KEY = "a29391c9413f10f2432ae70976895c6c"
+
+let lat = "35"
+let lon = "139"
+let currentUrl = "\(BASE_URL)\(lat)&lon=\(lon)&appid=\(API_KEY)"
+
 
