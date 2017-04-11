0040197f2e7311ce4eb4362f65bd1f8653508430
sherminb <Sherea@apples-MacBook-Pro.local>
2017-04-11 16:22:19 -0600

    Initial setup for table view in the controller

---
+++ WeatherApp/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Kuala on 2017-04-11.
+//  Copyright © 2017 Litroom. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL="https"
+let APPLICATION_ID=""
+
--- WeatherApp/MainVC.swift
+++ WeatherApp/MainVC.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource=self
+        tableView.delegate=self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
 
 }
 
